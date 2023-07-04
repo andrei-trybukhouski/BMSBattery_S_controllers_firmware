5fbe29cc65b7665248e20c41a3561661cec3fdca
Andrei Trybukhouski <Andrei.Trybukhouski@gmail.com>
2023-07-04 09:30:59 +0300

    motor starts

--- config.h
+++ config.h
@@ -9,17 +9,17 @@
 #define CONFIG_H_
 
 #define NUMBER_OF_PAS_MAGS 8
-#define limit 25
+#define limit 40
 #define timeout 3125
 #define wheel_circumference 1650L
-#define limit_without_pas 6
-#define ADC_THROTTLE_MIN_VALUE 80
+#define limit_without_pas 40
+#define ADC_THROTTLE_MIN_VALUE 46
 #define ADC_THROTTLE_MAX_VALUE 185
 #define BATTERY_VOLTAGE_MIN_VALUE 126
 #define BATTERY_CURRENT_MAX_VALUE 130L
 #define PHASE_CURRENT_MAX_VALUE 200L
-#define REGEN_CURRENT_MAX_VALUE -100L
-#define MOTOR_ROTOR_DELTA_PHASE_ANGLE_RIGHT 0
+#define REGEN_CURRENT_MAX_VALUE 100L
+#define MOTOR_ROTOR_DELTA_PHASE_ANGLE_RIGHT 215
 #define current_cal_a 100
 #define LEVEL_1 10
 #define LEVEL_2 20
@@ -32,22 +32,21 @@
 #define RAMP_END 1500
 #define P_FACTOR 0.5
 #define I_FACTOR 0.1
-#define GEAR_RATIO 1L
+#define GEAR_RATIO 23L
 #define PAS_THRESHOLD 1.7
 #define RAMP_START 64000
 #define limit_with_throttle_override 40
 #define CORRECTION_AT_ANGLE 127
-#define DISPLAY_TYPE_KT_LCD3
 #define ANGLE_4_0 1
 #define ANGLE_6_60 43
 #define ANGLE_2_120 86
 #define ANGLE_3_180 128
+#define BLUOSEC
 #define ANGLE_1_240 171
 #define ANGLE_5_300 213
 #define TQS_CALIB 0.0
-#define ACA 1432
-#define EEPROM_NOINIT // eeprom will not be cleared
-#define EEPROM_INIT_MAGIC_BYTE 191 // makes sure (chance of fail 1/255) eeprom is invalidated after flashing new config
+#define ACA 128
+#define EEPROM_INIT_MAGIC_BYTE 137 // makes sure (chance of fail 1/255) eeprom is invalidated after flashing new config
 #define ADC_BATTERY_VOLTAGE_K 69
 #define ACA_EXPERIMENTAL 128
 #define BATTERY_VOLTAGE_MAX_VALUE 156
